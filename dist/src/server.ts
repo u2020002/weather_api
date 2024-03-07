eb4b67f67cd1857409459294d1f88d2058ee1ae9
u2020002 <62593838+u2020002@users.noreply.github.com>
2024-03-07 16:03:58 +0000

    maqny

--- dist/src/server.ts
+++ dist/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import weatherRoute from './routes/weatherRoutes.js';
+import cors from "cors";
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 //our new route
 app.use('/api/weather', weatherRoute);
