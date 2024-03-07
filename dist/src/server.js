6f8803d4aa30c80b5e3e2f9771d9b7d103e65f43
u2020002 <62593838+u2020002@users.noreply.github.com>
2024-03-07 16:11:08 +0000

    ssss

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,19 +1,17 @@
-'use strict';
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-const weatherRoutes_js_1 = __importDefault(
-  require('./routes/weatherRoutes.js'),
-);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const weatherRoutes_js_1 = __importDefault(require("./routes/weatherRoutes.js"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = 3000;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 //our new route
 app.use('/api/weather', weatherRoutes_js_1.default);
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
